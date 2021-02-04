459b486ef1dbaeed8f391e6d402139cef2b487d4
yaotainan <yaotainan@jd.com>
2021-02-04 20:36:46 +0800

    dev

--- src/leetcode/editor/cn/entity/ListNode.java
+++ src/leetcode/editor/cn/entity/ListNode.java
@@ -11,4 +11,24 @@ public class ListNode {
     public ListNode next;
     public ListNode(int x) { val = x; }
     public ListNode(int x, ListNode node) {val = x; next = node;}
+
+    public static ListNode of(int[] num){
+        if(num.length == 0) return null;
+        ListNode head = new ListNode(num[0]), p = head;
+        for(int i = 1; i < num.length; i++){
+            ListNode temp = new ListNode(num[i]);
+            p.next = temp;
+            p = p.next;
+        }
+        return head;
+    }
+
+    public static String nodeValue(ListNode head){
+        StringBuilder sb = new StringBuilder();
+        while(head != null){
+            sb.append(head.val).append(",");
+            head = head.next;
+        }
+        return sb.toString();
+    }
 }
